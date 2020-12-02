6cf3c391b55041ccae3fef59d2a3123073517cc5
edmundo papi <edmundopierre@yahoo.com>
2020-12-02 17:03:21 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,5 +3,7 @@ class BirdsController < ApplicationController
     @birds = Bird.all
   #   render 'birds/index.html.erb'
   # end
+
+render json: { message: 'Hashes of data will get converted to JSON' }
   render plain: "Hello #{@birds[3].name}"
 end
